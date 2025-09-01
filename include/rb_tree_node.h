42f00d9fb3b45ea2694ecc49801ea77d7fad2625
Khachik Hovakimyan <khovakim@c1r2s1.42yerevan.am>
2025-09-01 15:41:57 +0400

    finished rb_tree node

--- include/rb_tree_node.h
+++ include/rb_tree_node.h
@@ -5,20 +5,22 @@
 
 namespace cxx {
 
-  ///! @struct rb_tree_node
-  ///! @brief Node structure for Red-Black Tree, templated by value type.
+  /// @struct rb_tree_node
+  /// @brief Node structure for Red-Black Tree, templated by value type.
   ///
-  ///! Inherits from rb_tree_base_node and stores a value of type ValueType.
+  /// Inherits from rb_tree_base_node and stores a value of type ValueType.
   ///
-  ///! @tparam ValueType The type of value stored in the node.
+  /// @tparam ValueType The type of value stored in the node.
   template <typename ValueType>
   struct rb_tree_node : public rb_tree_base_node
   {
-    ValueType _value { }; ///< Value stored in the node.
+    ValueType _value; ///< Value stored in the node.
 
-    ///! @brief Constructs a new rb_tree_node with the given value.
-    ///! @param value The value to store in the node.
-    explicit rb_tree_node(const ValueType& value);
+    /// @brief Constructs a new rb_tree_node with the given value.
+    /// @param value The value to store in the node.
+    explicit rb_tree_node(const ValueType& value)
+      : _value { value }
+    { }
   };
 
 }
